51f5b547bac2a088659b0dc18eb7c48a40760c43
rangga-18 <raditiarangga96@gmail.com>
2025-09-23 15:59:57 +0800

    Add files via upload

--- web-galeri-foto-rangga/proses_login.php
+++ web-galeri-foto-rangga/proses_login.php
@@ -1,1 +1,15 @@
-<?php
+<?php 
+session_start(); 
+include("koneksi.php"); 
+$username = $_POST['username']; 
+$password = $_POST['password']; 
+$sql = "SELECT * FROM user WHERE username='$username' AND password='$password'"; 
+$result = $conn->query($sql); 
+if ($result->num_rows > 0) { 
+ $_SESSION['username'] = $username; 
+ header("Location: welcome.php"); 
+} else { 
+ echo "Login gagal. <a href='index.php'>Coba lagi</a>"; 
+} 
+$conn->close(); 
+?>
